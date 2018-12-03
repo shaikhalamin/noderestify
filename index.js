0e5cd2acad52abf86a4347b93e7ed2476ae59c16
saminta <samin@teamamericany.com>
2018-12-03 14:41:58 +0600

    mongo db connection url changed to localhost

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ server.use(restify.plugins.queryParser());
 server.listen(3000,()=>{
     //console.log(process.env.MONGODB_URI);
     //mongoose.set('countDocuments', false);
-    mongoose.connect('mongodb://shaikhalamin:'+process.env.MONGO_PASSWORD+'@cluster0-shard-00-00-ejzdu.mongodb.net:27017,cluster0-shard-00-01-ejzdu.mongodb.net:27017,cluster0-shard-00-02-ejzdu.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',{ useNewUrlParser: true });
+    //mongoose.connect('mongodb://shaikhalamin:'+process.env.MONGO_PASSWORD+'@cluster0-shard-00-00-ejzdu.mongodb.net:27017,cluster0-shard-00-01-ejzdu.mongodb.net:27017,cluster0-shard-00-02-ejzdu.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',{ useNewUrlParser: true });
+    mongoose.connect('mongodb://localhost:27017/history',{ useNewUrlParser: true });
     console.log(`Server started on port`+3000);
 });
 
